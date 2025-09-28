d86df1fa91c88e62d3cb3c70f293f91aae84cb0d
Sepl-flek <154527662+Sepl-flek@users.noreply.github.com>
2025-09-28 19:16:04 +0300

    make an authentication system

---
+++ chatserver/account/views.py
@@ -0,0 +1,35 @@
+from django.contrib.auth import login, authenticate
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+
+from account.forms import SignUpForm, LoginForm
+
+
+def signup_view(request):
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("home")
+    else:
+        form = SignUpForm()
+    return render(request, "account/signup.html", {"form": form})
+
+
+def login_view(request):
+    form = LoginForm(data=request.POST or None)
+    if request.method == "POST":
+        if form.is_valid():
+            username = form.cleaned_data["username"]
+            password = form.cleaned_data["password"]
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect("home")
+
+    return render(request, "account/login.html", {"form": form})
+
+
+def home(request):
+    return HttpResponse("Дом")
--- chatserver/chatserver/settings.py
+++ chatserver/chatserver/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'daphne',
     'chat',
+    'account',
 
     'django.contrib.admin',
     'django.contrib.auth',
